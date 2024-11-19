df925a6ca7a2e49ed5b49dc9057ef93ac97e06d6
dunmas <levafork@gmail.com>
2024-11-19 23:39:43 +0300

    Возврат к исходной задумке - использование gRPC. Начал с малого - реализовал --help

--- src/mgmt_server.rs
+++ src/mgmt_server.rs
@@ -1,3 +1,24 @@
+use clap::{Arg, Command};
+
 fn main() {
-    
+    let matches = Command::new("rSOC")
+        .version("0.0.1")
+        .author("buran <bvran@proton.me>")
+        .about("rSOC - Simple network and endpoint SOC implementation written on Rust
+
+This is a Management Server - head unit of SOC")
+        .arg(Arg::new("user")
+                 .short('u')
+                 .long("user")
+                 .help("User to authentificate"))
+        .arg(Arg::new("password")
+                 .short('p')
+                 .long("password")
+                 .help("User's password"))
+        .get_matches();
+
+    let username = matches.get_one::<String>("user").unwrap();
+    let password = matches.get_one::<String>("password").unwrap();
+
+    println!("{} and {}", username, password);
 }
