d1003021d0b2a387e42fa2749d72a531c2309799
dunmas <levafork@gmail.com>
2024-11-21 18:50:48 +0300

    Начал работу с интерфейса. Реализовал главное меню.

--- src/mgmt_server.rs
+++ src/mgmt_server.rs
@@ -1,12 +1,65 @@
 use clap::{Arg, Command};
+use std::io::{self, Write};
+
+const USER_LIST_FILE: &str = "users.txt";
+const AUDIT_LOG: &str = "audit.txt";
+const EVENT_LOG: &str = "events.txt";
+
+const MAIN_MENU: &str = "\
+        ------------------------------------------------------\n\
+        Hello! This is rSOC Management Server command console.\n\
+        ------------------------------------------------------\n\
+        Please, select option:\n\
+        1) Event log               4) Users settings\n\
+        2) Sensors settings        5) Exit\n\
+        3) Audit settings\n\
+        ------------------------------------------------------";
+const EVENT_MENU: &str = "Select option:\n \
+        1) Check overall events (10 latest)\n \
+        2) Check overall events (10 latest)\n \
+        3) Back\n";
+const SENSORS_MENU: &str = "Select option:\n \
+        1) List of sensors\n \
+        2) Start/stop sensor\n \
+        3) Update rules\n \
+        4) Back\n";
+const USERS_MENU: &str = "Select option:\n \
+        1) List of users\n \
+        2) Update user\n \
+        3) Delete user\n \
+        4) Back\n";
+
+fn get_user_choice() -> String {
+    let mut choice = String::new();
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut choice).expect("Input error.");
+    choice.trim().to_string()
+}
+
+fn main_menu() {
+    loop {
+        println!("{}", MAIN_MENU);
+        let choise = get_user_choice();
+
+        match choise.as_str() {
+            "1" => println!("Foo"),
+            "2" => println!("Foo"),
+            "3" => println!("Foo"),
+            "4" => println!("Foo"),
+            "5" => {
+                println!("Goodbye.");
+                break;
+            },
+            _ => println!("Undefined option. Try again.\n"),
+        }
+    }
+}
 
 fn main() {
     let matches = Command::new("rSOC")
         .version("0.0.1")
         .author("buran <bvran@proton.me>")
-        .about("rSOC - Simple network and endpoint SOC implementation written on Rust
-
-This is a Management Server - head unit of SOC")
+        .about("rSOC - Simple network and endpoint SOC implementation written on Rust\n\nThis is a Management Server - head unit of SOC")
         .arg(Arg::new("user")
                  .short('u')
                  .long("user")
@@ -17,8 +70,9 @@ This is a Management Server - head unit of SOC")
                  .help("User's password"))
         .get_matches();
 
-    let username = matches.get_one::<String>("user").unwrap();
-    let password = matches.get_one::<String>("password").unwrap();
+    // let username = matches.get_one::<String>("user").unwrap();
+    // let password = matches.get_one::<String>("password").unwrap();
+    // println!("{} and {}", username, password);
 
-    println!("{} and {}", username, password);
+    main_menu();
 }
