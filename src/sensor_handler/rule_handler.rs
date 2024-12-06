c318a0c161ea99e3412d93046a51431c940fbab8
buran <bvran@proton.me>
2024-12-06 10:07:07 +0300

    Реализовал функцию добавления правила

--- src/sensor_handler/rule_handler.rs
+++ src/sensor_handler/rule_handler.rs
@@ -1,7 +1,65 @@
-use std::borrow::Borrow;
 use std::collections::HashMap;
 use std::fs::OpenOptions;
 use std::io::{Read, Seek, Write};
+use std::hash::Hash;
+use sha2::{Sha256, Digest};
+
+pub fn get_rules_list(rule_type: &str, rules_file: &String) {
+    if let Some(rules_vec_by_level) =  get_rules_map(rules_file).get(rule_type) {
+        for level_rule in rules_vec_by_level {
+            let rule_hash_vec: Vec<String> = level_rule.clone().into_keys().collect();
+            let rule_hash: &String = rule_hash_vec.first().unwrap();
+            println!("------------------------------------------------------------------------------------------\n\
+                      Rule level: {}\n\
+                      Rule hash: {}\n", rule_type, rule_hash);
+
+            for param in level_rule.get(rule_hash).unwrap() {
+                println!("{}: {}", param.0, param.1);
+            }
+        }
+    }
+}
+
+pub fn add_rule(rule_level: &str, rule_name: &str, rule_payload: &str, rule_fields: &HashMap<&str, &str>, rules_file: &str) -> bool {
+    let mut rules_file = OpenOptions::new()
+                            .append(true)
+                            .create(true)
+                            .read(true)
+                            .open(rules_file)
+                            .unwrap();
+
+    let hashing_str = rule_name.to_string() + rule_payload;
+    let mut hasher = Sha256::new();
+    hasher.update(hashing_str);
+    let raw_hash = hasher.finalize();
+    let hash = format!("{:.5}", format!("{:x}", raw_hash));
+
+    let mut param_pairs: HashMap<&str, &str> = vec![
+        ("level", rule_level),
+        ("hash", &hash),
+        ("name", rule_name),
+        ("payload", rule_payload)
+    ].into_iter().collect();
+
+    param_pairs.extend(rule_fields);
+    let mut param_vec: Vec<String> = Vec::new();
+    
+    for pair in param_pairs {
+        let joined_string = pair.0.to_string() + "[:1:]" + pair.1;
+        param_vec.push(joined_string);
+    }
+
+    let result = match writeln!(rules_file, "{}", param_vec.join("[:2:]")) {
+        Ok(_) => true,
+        Err(_e) => false
+    };
+
+    result
+}
+
+pub fn delete_rule(rule_level: &String, rule_hash: &String, rules_file: &String) {
+
+}
 
 fn get_rules_map(rules_file: &String) -> HashMap<String, Vec<HashMap<String, Vec<(String, String)>>>> {
     let mut file = OpenOptions::new()
@@ -48,28 +106,4 @@ fn get_rules_map(rules_file: &String) -> HashMap<String, Vec<HashMap<String, Vec
     }
 
     result
-}
-
-pub fn get_rules_list(rule_type: &str, rules_file: &String) {
-    if let Some(rules_vec_by_level) =  get_rules_map(rules_file).get(rule_type) {
-        for level_rule in rules_vec_by_level {
-            let rule_hash_vec: Vec<String> = level_rule.clone().into_keys().collect();
-            let rule_hash: &String = rule_hash_vec.first().unwrap();
-            println!("------------------------------------------------------------------------------------------\n\
-                      Rule level: {}\n\
-                      Rule hash: {}\n", rule_type, rule_hash);
-
-            for param in level_rule.get(rule_hash).unwrap() {
-                println!("{}: {}", param.0, param.1);
-            }
-        }
-    }
-}
-
-pub fn add_rule(rule_level: &str, rule_name: &str, rule_payload: &str, rule_fields: &HashMap<&str, &str>, rules_file: &str) {
-
-}
-
-pub fn delete_rule(rule_level: &String, rule_hash: &String, rules_file: &String) {
-
 }
